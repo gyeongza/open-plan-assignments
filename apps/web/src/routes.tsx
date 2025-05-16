20c9e07f92f7ed37112ff40dc520b60bad32c5d5
gyeongza <gyeongza@naver.com>
2025-05-16 14:07:14 +0900

    feat: query client 및 Result page route 추가

--- apps/web/src/routes.tsx
+++ apps/web/src/routes.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
+import Result from './pages/Result';
 import Layout from './components/Layout';
 
-// 라우트 설정
 const routes = [
   {
     path: '/',
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
+      { path: 'result', element: <Result /> },
     ],
   },
 ];
@@ -17,4 +18,5 @@ export const router = createBrowserRouter(routes);
 
 export const ROUTES = {
   HOME: '/',
+  RESULT: '/result',
 };
