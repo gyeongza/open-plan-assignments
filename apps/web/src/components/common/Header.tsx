287a60498b0fbc46109f1d62d07b6b5a8c8f0133
gyeongza <gyeongza@naver.com>
2025-05-16 18:27:43 +0900

    feat: 조회 이력 초기화 버튼 추가

--- apps/web/src/components/common/Header.tsx
+++ apps/web/src/components/common/Header.tsx
@@ -1,13 +1,27 @@
 import { useLocation } from 'react-router-dom';
 import { APPLICANT_NAME } from '../../constants';
 import { ROUTES } from '../../routes';
+import { usePhotoStore } from '../../store/photoStore';
+import { Button } from '@repo/ui';
 
 export default function Header() {
+  const { clearPhotoDetail } = usePhotoStore();
   const isHome = useLocation().pathname === ROUTES.HOME;
 
+  const handleClearPhotoDetail = () => {
+    clearPhotoDetail();
+  };
+
   return (
-    <div className="flex items-center justify-center px-5 py-[17px]">
-      <span className={`text-center text-sm ${!isHome && 'text-white'}`}>{APPLICANT_NAME}</span>
+    <div className="relative flex items-center px-5 py-[17px]">
+      <span className={`${!isHome && 'text-white'} absolute left-1/2 -translate-x-1/2 text-center text-sm`}>
+        {APPLICANT_NAME}
+      </span>
+      <div className="ml-auto">
+        <Button className="h-8" onClick={handleClearPhotoDetail}>
+          조회이력 초기화
+        </Button>
+      </div>
     </div>
   );
 }
