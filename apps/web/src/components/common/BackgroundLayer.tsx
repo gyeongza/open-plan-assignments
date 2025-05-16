679d521b52238dcf28e324e2b53c2b5477b86986
gyeongza <gyeongza@naver.com>
2025-05-16 17:15:38 +0900

    design: 배경이미지 추가

--- apps/web/src/components/common/BackgroundLayer.tsx
+++ apps/web/src/components/common/BackgroundLayer.tsx
@@ -1,8 +1,16 @@
+import { usePhotoStore } from '../../store/photoStore';
+
 export default function BackgroundLayer() {
+  const { photoDetail } = usePhotoStore();
+
   return (
-    <div className="fixed inset-0 -z-10">
+    <div className="fixed inset-0 z-10">
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-30"
+        style={{ backgroundImage: `url(${photoDetail?.download_url})` }}
+      />
       <div className="absolute inset-0 bg-gradient-to-b from-[#D9D9D9] to-transparent" />
-      <div className="absolute inset-0 bg-black/30" />
+      <div className="absolute inset-0 bg-black/50" />
     </div>
   );
 }
